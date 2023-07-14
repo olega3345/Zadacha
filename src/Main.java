bd6554a0fa618beccd2636487af338e5c2116658
Олег <olegcelysev778@gmail.com>
2023-07-14 12:16:38 +0300

    Коммит

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+class main {
+    public static void main(String[] args) {
+        returningArr(new int[]{1, 2, 3, 4, 5, 6, 7});
+    }
+    public static int [] returningArr (int [] array) {
+        for (int i = 2; i < 7; i++) {
+            if (array[i] % 2 == 0) {
+                array[i]++;
+            }
+            System.out.println(array[i]);
+        }
+        return array;
+    }
+}
